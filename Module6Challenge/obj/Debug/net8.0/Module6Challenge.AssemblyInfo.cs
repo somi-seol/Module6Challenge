a5d82ab110ee04d9eb8100b20dcbd3fcc209c323
Somi Seol <tl3222zx@go.minnstate.edu>
2025-02-18 23:58:17 +0000

    updated style and social links

--- Module6Challenge/obj/Debug/net8.0/Module6Challenge.AssemblyInfo.cs
+++ Module6Challenge/obj/Debug/net8.0/Module6Challenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module6Challenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932482a51422af4adf10bceedefed298cd187e79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5598c51520a3dd3cb3275ef2dad7f64a9f2e356a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module6Challenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module6Challenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
